831c555a9c212cee2bb4c810dc81a7615274b0a9
maikeljh <mjh191003@gmail.com>
2025-02-03 10:15:36 +0700

    [New Feature] Initial Commit

---
+++ common/config.py
@@ -0,0 +1,4 @@
+class RewriterPromptConfig:
+    type: str
+    api_key: str
+    model_path: str
---
+++ core/rewriter.py
@@ -0,0 +1,5 @@
+from common import RewriterPromptConfig
+
+class RewriterPrompt:
+    def __init__(self, config: RewriterPromptConfig):
+        self.config = config
